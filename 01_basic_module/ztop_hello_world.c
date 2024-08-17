79c44778ac1f4e693da4dd63b24e5af2db4b8785
habebe <hay.knock@gmail.com>
2024-08-17 16:59:18 -0500

    added module parameters.

--- 01_basic_module/ztop_hello_world.c
+++ 01_basic_module/ztop_hello_world.c
@@ -1,12 +1,26 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/sched.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ztop");
 MODULE_DESCRIPTION("Hello world module");
 
+static char* param_charp = "default_charp";
+static int param_int = 1;
+
+module_param(param_charp,charp,S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+module_param(param_int,int,S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+
+MODULE_PARM_DESC(param_charp, "A charp parameter");
+MODULE_PARM_DESC(param_int, "An int parameter");
+
 static __init int initialize(void)
 {
+    printk(KERN_EMERG "[ztop][hello_module] initialize param=(param_charp:'%s' param_int:%d)",param_charp,param_int);
+
+
     printk(KERN_EMERG "[ztop][hello_module] initialize (KERN_EMERG)");
     printk(KERN_ALERT "[ztop][hello_module] initialize (KERN_ALERT)");
     printk(KERN_CRIT "[ztop][hello_module] initialize (KERN_CRIT)");
@@ -17,8 +31,8 @@ static __init int initialize(void)
     printk(KERN_INFO "[ztop][hello_module] initialize (KERN_INFO)");
     printk(KERN_DEBUG "[ztop][hello_module] initialize (KERN_DEBUG)");
     printk(KERN_DEFAULT "[ztop][hello_module] initialize (KERN_DEFAULT)");
-    printk(KERN_CONT "[ztop][hello_module] initialize (KERN_CONT)");
-    printk("[ztop][hello_module] initialize");
+
+    printk(KERN_INFO "[ztop][hello_module] initialize process(%s,%d)",current->comm,current->pid);
     return 0;
 }
 
